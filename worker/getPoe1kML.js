6f9188beb0d2109cf5d5ff3eaa3d00f1dca845b8
realJingYang <jingyang@iisjy.cn>
2020-06-09 22:26:13 +0800

    renew code

--- worker/getPoe1kML.js
+++ worker/getPoe1kML.js
@@ -1,7 +1,5 @@
-const getPoeML = poetryObj => {
+addEventListener('message', e => {
     let mlStr = "";
-    poetryObj.forEach(elem => {mlStr+=`<div class="cardBox"><div class="headerBox"><p name="gs-id" style="display: none;">${elem.id}</p><p name="gs-title">${elem.title}</p><p><span name="gs-dynasty">${elem.dynasty}</span>：<a name="gs-author" href="" class="headerBox-label">${elem.author}</a></p></div><div class="bodyBox" name="gs-text">${elem.text}</div></div>`});
+    e.data.forEach(elem => {mlStr+=`<div class="cardBox"><div class="headerBox"><p name="gs-id" style="display: none;">${elem.id}</p><p name="gs-title">${elem.title}</p><p><span name="gs-dynasty">${elem.dynasty}</span>：<a name="gs-author" href="" class="headerBox-label">${elem.author}</a></p></div><div class="bodyBox" name="gs-text">${elem.text}</div></div>`});
     self.postMessage(mlStr);
-}
-
-addEventListener('message', e => getPoeML(e.data), false);
+}, false);
