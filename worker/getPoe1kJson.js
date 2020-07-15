7c4cfc0fcfbd9646c3eb1ce6411aa5e2ca5b2913
realJingYang <jingyang@iisjy.cn>
2020-07-15 23:18:26 +0800

    添加CDN版本号

--- worker/getPoe1kJson.js
+++ worker/getPoe1kJson.js
@@ -1,6 +1,6 @@
 const vers = "v1.1.2"
 addEventListener('message', e => {
-    for (let i = e.data.offset; i < 1254; i+=e.data.step) {
+    for (let i = e.data.offset; i < 126; i+=e.data.step) {
         fetch(`https://cdn.jsdelivr.net/gh/libpoetry/data@${vers}/grouped/${i.toString()}.json`)
             .then(resp => resp.json())
             .then(poetryObj => self.postMessage(poetryObj))
