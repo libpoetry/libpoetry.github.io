3e10e7332f3ae63a98fc6bb5791990a9e37fe907
realJingYang <jingyang@iisjy.cn>
2020-07-15 11:29:38 +0800

    Use CDN

--- worker/getPoe1kJson.js
+++ worker/getPoe1kJson.js
@@ -1,6 +1,6 @@
 addEventListener('message', e => {
     for (let i = e.data.offset; i < 1254; i+=e.data.step) {
-        fetch(`https://cdn.jsdelivr.net/gh/libpoetry/data/poe1k//${i.toString()}.json`)
+        fetch(`https://cdn.jsdelivr.net/gh/libpoetry/data/poe1k/${i.toString()}.json`)
             .then(resp => resp.json())
             .then(poetryObj => self.postMessage(poetryObj))
             .catch(e => console.log(i.toString()+':'+e));
